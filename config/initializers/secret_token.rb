f06bc1c3ef07c73ce6126a7d018e63c3b8846b10
Haroon Khan <haroonkhan79@gmail.com>
2012-04-22 17:53:28 +0100

    rearranging the project structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '4e9936a015b9e1f83a7da8148f4318420e5deebfdda88865d6f356289c6d8e24edc0c6170f0d176701efac0718906fb151330f248e0c07d5e70ed0bd2dc05f11'
